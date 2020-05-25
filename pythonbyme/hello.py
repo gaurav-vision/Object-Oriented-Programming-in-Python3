7d4dee9d1ac3c0eb1813eb444f38a35ca0bd67cb
Gaurav Kumar Yadav <45019238+gaurav-vision@users.noreply.github.com>
2020-05-25 18:12:41 +0530

    Add files via upload

---
+++ pythonbyme/hello.py
@@ -0,0 +1,4 @@
+from newdir import myFunction
+
+print(myFunction.add(3, 8))
+print(myFunction.multiply(3,8))
---
+++ pythonbyme/shape.py
@@ -0,0 +1,7 @@
+class Shape:
+    color = None
+
+    def set_color(self, color):
+        self.__color = color
+    def get_color(self):
+        return self.__color
